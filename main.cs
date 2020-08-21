6dfbd457cad6121be5a6ea86eddea106d025598b
g3l4d31r4 <luccabaeta@hotmail.com>
2020-08-21 17:44:56 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,14 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+    Console.Write("Informe o primeiro numero:");
+    int num1 = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Informe o segundo numero: ");
+    int num2 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("A soma de {0} + {1} é = {2}", num1,num2,num1+num2);
+    }
+}
+
+      
+
